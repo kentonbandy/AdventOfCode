c0cdfa1e9dab7609dbe7272b676ec831e916d6d0
kenny10293847 <thetrumpetplayer@gmail.com>
2022-12-04 23:33:54 -0500

    2022 day 4 python

---
+++ 2022/python/solutions/04.py
@@ -0,0 +1,17 @@
+import file_reader
+
+lines = file_reader.get_lines('2022/python/inputs/04.txt')
+count = 0
+count2 = 0
+
+for line in lines:
+    assignments = line.split(',')
+    a = [int(c) for c in assignments[0].split('-')]
+    b = [int(c) for c in assignments[1].split('-')]
+    if (a[0] >= b[0] and a[1] <= b[1]) or (b[0] >= a[0] and b[1] <= a[1]):
+        count += 1
+    if (a[0] <= b[0] and a[1] >= b[0]) or (b[0] <= a[0] and b[1] >= a[0]):
+        count2 += 1
+
+print(count)
+print(count2)
