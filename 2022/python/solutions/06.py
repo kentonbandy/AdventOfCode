4afa5fe7190191d5586571a683dfac6a406ac1b8
kenny10293847 <thetrumpetplayer@gmail.com>
2022-12-06 23:33:52 -0500

    2022 day 6 python

--- 2022/python/solutions/06.py
+++ 2022/python/solutions/06.py
@@ -2,3 +2,14 @@ import file_reader
 
 file_num = __file__.split("\\")[-1].split('.')[0]
 lines = file_reader.get_lines(file_num)
+inpt = lines[0]
+
+def find_first_marker(count, inpt):
+    for i in range(len(inpt)):
+        word = inpt[i:i+count]
+        newword = ''.join(set(word))
+        if len(newword) == count:
+            return i + count
+
+print(find_first_marker(4, inpt))
+print(find_first_marker(14, inpt))
