41a46e3437d9b4cebf83493f7f76f6622f90f403
kenny10293847 <thetrumpetplayer@gmail.com>
2022-12-10 11:08:18 -0500

    visualizer for 2022 day 9

--- 2022/python/solutions/09.vis.py
+++ 2022/python/solutions/09.vis.py
@@ -1,15 +1,15 @@
 import file_reader
+import time
+import os
 
 lines = list(map(lambda l: [l[0],int(l[1])], [line.split(' ') for line in file_reader.get_lines(__file__)]))
+
+maxx, maxy = 0,0
 
 class Knot:
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.pos = set([(0,0)])
-
-    def mark(self):
-        self.pos.add((self.x,self.y))
 
     def follow(self, knot):
         x,y = 0,0
@@ -19,7 +19,6 @@ class Knot:
         # clamp x and y changes to -1:1
         self.x += sorted([-1, x, 1])[1]
         self.y += sorted([-1, y, 1])[1]
-        self.mark()
 
 knots = [Knot() for _ in range(10)]
 head = knots[0]
@@ -34,5 +33,14 @@ for [d, m] in lines:
             knots[i+1].follow(knots[i])
         m -= 1
 
-print(len(knots[1].pos)) # part 1
-print(len(knots[9].pos)) # part 2
+        # visualizer
+        frame = [['.' for _ in range(21)] for _ in range(21)]
+        for knot in knots:
+            x = knot.x - head.x + 10
+            y = knot.y - head.y + 10
+            frame[y][x] = '#'
+        frame = [' '.join(line) for line in frame]
+        os.system('cls')
+        for line in frame:
+            print(line)
+        time.sleep(0.05)
