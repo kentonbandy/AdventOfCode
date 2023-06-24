cc654a3011ee0c6570e83b770c9859243160bc93
kenny10293847 <thetrumpetplayer@gmail.com>
2023-06-24 17:03:03 -0400

    ok fine 2015 day 4

--- 2015/python/solutions/04.py
+++ 2015/python/solutions/04.py
@@ -1,5 +1,20 @@
 import file_reader
+import hashlib
 
 secret_key = file_reader.get_lines(__file__)[0]
 
-#oh no
+
+def gethash(key, numzeroes):
+    num = 1
+    matchstring = '0' * numzeroes
+    while True:
+        bytestring = f'{key}{num}'.encode('utf-8')
+        hexstring = hashlib.md5(bytestring).hexdigest()
+        if hexstring[:numzeroes] == matchstring:
+            break
+        num += 1
+    print(num)
+
+
+gethash(secret_key, 5)
+gethash(secret_key, 6)
