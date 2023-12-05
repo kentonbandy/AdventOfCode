c0eea280c5200c4defd6d50c1b2ace4445d82268
kenny10293847 <thetrumpetplayer@gmail.com>
2023-12-04 23:28:41 -0500

    add a grid function and update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AdventOfCode
----
+
 My solutions for the [Advent of Code](https://adventofcode.com/) problems.
+
+## Languages used
+
+C#, Python, JavaScript, Go, AXIOM (assembly language from [Exapunks](https://www.zachtronics.com/exapunks/))
+
+For my brief attempt at the 2023 problems in Golang, see [my aoc-go repository](https://github.com/kentonbandy/aoc-go).
---
+++ jshelpers/GridFuncs.js
@@ -0,0 +1,43 @@
+/**
+ * Returns the values of the neighbors of a given grid cell as an object.
+ * If cardinal is true, only the 4 cardinal directions are returned.
+ * If a callback is provided, it is called on each neighbor value.
+ * @param {Array} grid 
+ * @param {int} x 
+ * @param {int} y 
+ * @param {bool} cardinal 
+ * @param {function} callback
+ * @returns 
+ */
+export function getNeighbors(grid, x, y, cardinal = false, callback = null) {
+  let u, ur, r, dr, d, dl, l, ul;
+
+  const lookup = y > 0;
+  const lookright = x < grid.length - 1;
+  const lookdown = y < grid.length - 1;
+  const lookleft = x > 0;
+
+  if (lookup) u = { val: grid[y-1][x], x, y: y-1 };
+  if (lookright) r = { val: grid[y][x+1], x: x+1, y };
+  if (lookdown) d = { val: grid[y+1][x], x, y: y+1 };
+  if (lookleft) l = { val: grid[y][x-1], x: x-1, y };
+
+  let neighbors = { u, r, d, l };
+
+  if (!cardinal) {
+    if (lookup && lookright) ur = { val: grid[y-1][x+1], x: x+1, y: y-1 };
+    if (lookright && lookdown) dr = { val: grid[y+1][x+1], x: x+1, y: y+1 };
+    if (lookdown && lookleft) dl = { val: grid[y+1][x-1], x: x-1, y: y+1 };
+    if (lookup && lookleft) ul = { val: grid[y-1][x-1], x: x-1, y: y-1 };
+
+    neighbors = { ...neighbors, ur, dr, dl, ul };
+  }
+
+  if (callback) {
+    for (const neighbor of Object.values(neighbors)) {
+      if (neighbor) callback(neighbor.val, neighbor.x, neighbor.y);
+    }
+  }
+
+  return neighbors;
+}
