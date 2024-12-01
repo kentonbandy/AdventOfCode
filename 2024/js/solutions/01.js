36ddead10ae3825a59e50bb7d98db6b0b6133709
Kenton Bandy <kentonbandy@Kentons-Mac-mini.local>
2024-12-01 08:50:33 -0500

    2024 day 1

--- 2024/js/solutions/01.js
+++ 2024/js/solutions/01.js
@@ -2,4 +2,21 @@ import { getInput } from '../../../jshelpers/InputGetter.js';
 
 const lines = await getInput(import.meta.url);
 
-console.log(lines);
+const linepairs = lines.map((line) => line.split('   '));
+const lst1 = linepairs.map(([id1, _]) => id1).toSorted((a, b) => a - b);
+const lst2 = linepairs.map(([_, id2]) => id2).toSorted((a, b) => a - b);
+let distance = 0;
+const lst2map = {};
+lst2.forEach((id) => lst2map[id] = isNaN(lst2map[id]) ? 1 : lst2map[id] + 1);
+let similarity = 0;
+
+// part 1: add differences at each index
+lst1.forEach((id, i) => distance += Math.abs(id - lst2[i]));
+// part 2: multiply each number in the left list by the number of times it appears in the right
+lst1.forEach((id) => similarity += id * (lst2map[id] ?? 0));
+
+console.log(distance);
+console.log(similarity);
+
+// 8:40
+// 23:42
