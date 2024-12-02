380d2aaae2a659e3a1f11fd38e9b718c0b733c44
kenny10293847 <thetrumpetplayer@gmail.com>
2024-12-01 22:03:50 -0500

    smol adjustments

--- 2024/js/solutions/01.js
+++ 2024/js/solutions/01.js
@@ -3,7 +3,7 @@ import { l } from '../../../jshelpers/functions.js'
 
 const lines = await getInput(import.meta.url);
 
-// part 1 
+// part 1
 const linepairs = lines.map((line) => line.split('   '));
 // separate and sort left and right lists
 const lst1 = linepairs.map(([id1, _]) => id1).toSorted((a, b) => a - b);
@@ -16,7 +16,7 @@ l(distance);
 const lst2map = {};
 lst2.forEach((id) => lst2map[id] = isNaN(lst2map[id]) ? 1 : lst2map[id] + 1);
 // similarity is id in left list * how many times it is found in the right list
-const similarity = lst1.reduce((sum, id) => sum += id * (lst2map[id] ?? 0), 0);
+const similarity = lst1.reduce((sum, id) => sum + (id * (lst2map[id] ?? 0)), 0);
 l(similarity);
 
 // 8:40
