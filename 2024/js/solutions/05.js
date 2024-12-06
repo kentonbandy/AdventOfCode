9e138f01f992dcda0365656990342ae99cf422e6
kenny10293847 <thetrumpetplayer@gmail.com>
2024-12-05 23:00:19 -0500

    2024 day 5 part 2

--- 2024/js/solutions/05.js
+++ 2024/js/solutions/05.js
@@ -21,20 +21,23 @@ const updateMaps = updateStrings.map((update) => {
   const map = {};
   for (const value of arr) map[value] = arr.indexOf(value);
   map['middle'] = arr[Math.floor(arr.length / 2)];
+  map['array'] = arr;
   return map;
 });
 
 l(validateUpdates(updateMaps, ruleMap));
 
 function validateUpdates(updates, rules) {
-  return updates.reduce((total, update) => {
-    return total + validateUpdate(update, rules);
-  }, 0);
+  return updates.reduce(([totalValid, totalFixed], update) => {
+    const valid = validateUpdate(update, rules);
+    const fixed = valid > 0 ? 0 : fixUpdate(update.array, rules);
+    return [totalValid + valid, totalFixed + fixed];
+  }, [0, 0]);
 }
 
 function validateUpdate(update, rules) {
   for (const [num, ind] of Object.entries(update)) {
-    if (num === 'middle') continue;
+    if (num === 'middle' || num === 'array') continue;
     if (!checkUpdateValue(ind, rules[num], update)) return 0;
   }
   return parseInt(update['middle']);
@@ -48,4 +51,21 @@ function checkUpdateValue(ind, rules, update) {
     if (afterNum in update && update[afterNum] > ind) return false;
   }
   return true;
+}
+
+function fixUpdate(updates, rules) {
+  const sorted = updates.toSorted((a, b) => {
+    const aRules = rules[a];
+    const bRules = rules[b];
+
+    // a definitely comes before b if either rule is met
+    if ((aRules?.after?.includes(b)) || (bRules?.before?.includes(a))) return 1;
+    // b definitely comes before a if either rule is broken
+    if ((aRules?.before?.includes(b) || (bRules?.after?.includes(a)))) return -1;
+    // otherwise we have no opinions
+    return 0;
+  });
+
+  // return middle value
+  return parseInt(sorted[Math.floor(sorted.length / 2)]);
 }
